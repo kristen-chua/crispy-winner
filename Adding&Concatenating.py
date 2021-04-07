a5fd998aab7ff690ef5b3cc80e6fcb3aff187eee
Kristen C <kcchua.chua@gmail.com>
2021-04-07 15:42:06 -0700

    Add files via upload

---
+++ Adding&Concatenating.py
@@ -0,0 +1,12 @@
+def Add(x,y):
+	z=y+x
+	return(y)
+	print(z)
+Add(1,2)
+
+#def my_function(fname):
+#  print(fname + " Refsnes")
+#
+#my_function("Emil")
+#my_function("Tobias")
+#my_function("Linus")##
