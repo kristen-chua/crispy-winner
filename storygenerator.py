c3b37faf802d1b7e62a6b147acdd48df33704c2e
Kristen Chua <kcchua.chua@gmail.com>
2021-04-07 16:51:22 -0700

    Create storygenerator.py

---
+++ storygenerator.py
@@ -0,0 +1,21 @@
+#https://dev.to/mindninjax/how-to-build-a-random-story-generator-using-python-1oah
+import random
+
+
+when = ['A long time ago', 'Yesterday', 'Before you were born', 'In future', 'Before Thanos arrived']
+who = ['Shazam', 'Iron Man', 'Batman', 'Superman', 'Captain America']
+where = ['Arkham Asylum', 'Gotham City', 'Stark Tower', 'Bat Cave', 'Avengers HQ']
+what = ['to eat a lot of cakes', 'to fight for justice', 'to steal ice cream', 'to dance']
+sequencewords = ['Next','Then','Afterward','Meanwhile']
+verb = ['went under','ran through','had fun at','went shopping in']
+#story starter
+print(random.choice(when) + ', ' + random.choice(who) + ' went to ' + random.choice(where) + ' ' + random.choice(what) + '.')
+
+#story body
+i=0
+for i in range(0,6):
+	print(random.choice(sequencewords)+', '+random.choice(who)+' ',random.choice(verb)+' '+random.choice(where)+' '+random.choice(what)+'.')	
+	i=i+1
+
+#how do I make it so that the sequencewords and actions do not repeat?
+#add conclusion sentence
